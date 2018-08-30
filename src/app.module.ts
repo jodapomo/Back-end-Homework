290d4006fa65374797eb13e26db5cee44fb3679c
Jose Posada <josedanielpm@gmail.com>
2018-08-29 21:10:14 -0500

    View cats and create cat working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    CatsModule,
+    MongooseModule.forRoot('mongodb://admin:admin123@ds133622.mlab.com:33622/homeworkdb', { useNewUrlParser: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
