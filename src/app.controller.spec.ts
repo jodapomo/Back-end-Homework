364aa4a532ef0dc15a607f3c5f675f8edf2cde82
Jose Posada <josedanielpm@gmail.com>
2018-08-30 22:19:21 -0500

    app controller test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -13,9 +13,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it("should return {'message': 'I'm ok!'}", () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.root()).toBe('Hello World!');
+      expect(appController.root()).toEqual({"message": "I'm ok!"});
     });
   });
 });
