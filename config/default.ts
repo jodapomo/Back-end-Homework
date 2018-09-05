66826c4baa8e5d1c36c6466e36328a454763cefa
Jose Posada <josedanielpm@gmail.com>
2018-09-04 23:00:20 -0500

    Fix

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
-    HOST: 'https://praxis-homework.herokuapp.com/',
+    HOST: 'https://praxis-homework.herokuapp.com',
     PORT: 3000,
     MONGO_URI: 'mongodb://admin:admin123@ds133622.mlab.com:33622/homeworkdb',
 }
