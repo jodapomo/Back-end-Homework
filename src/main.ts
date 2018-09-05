1d29cdfc7a2d076f0b2b7fc6e24532b7e6fca059
Jose Posada <josedanielpm@gmail.com>
2018-09-05 17:25:50 -0500

    Try 2: fix swagger

--- src/main.ts
+++ src/main.ts
@@ -8,45 +8,45 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // http://localhost:3000 : https://whateverwhatever.com
-  const hostDomain = AppModule.isDev ? `${AppModule.host}:${AppModule.port}`: AppModule.host;
+  // const hostDomain = AppModule.isDev ? `${AppModule.host}:${AppModule.port}`: AppModule.host;
 
-  const swaggerOptions = new DocumentBuilder()
-    .setTitle('Homework API')
-    .setDescription('API Documentation')
-    .setVersion('1.0.0')
-    .setHost(hostDomain.split('//')[1])
-    .setSchemes(AppModule.isDev ? 'http' : 'https')
-    .setBasePath('/api/v1')
-    .addBearerAuth('Authorization', 'header')
-    .build();
-
-  const swaggerDoc = SwaggerModule.createDocument(app, swaggerOptions);
-
-  app.use('/api/v1/docs/swagger.json', (req, res) => {
-    res.send(swaggerDoc);
-  });
-
-  SwaggerModule.setup('/api/v1/docs', app, null, {
-    swaggerUrl: `${hostDomain}/api/v1/docs/swagger.json`,
-    explorer: true,
-    swaggerOptions: {
-      docExpansion: 'list',
-      filter: true,
-      showRequestDuration: true,
-    },
-  });
-
-  //   const options = new DocumentBuilder()
-  //   .setTitle('Cats example')
-  //   .setDescription('The cats API description')
-  //   .setVersion('1.0')
-  //   .addTag('cats')
+  // const swaggerOptions = new DocumentBuilder()
+  //   .setTitle('Homework API')
+  //   .setDescription('API Documentation')
+  //   .setVersion('1.0.0')
+  //   .setHost(hostDomain.split('//')[1])
   //   .setSchemes(AppModule.isDev ? 'http' : 'https')
   //   .setBasePath('/api/v1')
-  //   .addBearerAuth()
+  //   .addBearerAuth('Authorization', 'header')
   //   .build();
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('/api/v1/docs', app, document);
+
+  // const swaggerDoc = SwaggerModule.createDocument(app, swaggerOptions);
+
+  // app.use('/api/v1/docs/swagger.json', (req, res) => {
+  //   res.send(swaggerDoc);
+  // });
+
+  // SwaggerModule.setup('/api/v1/docs', app, null, {
+  //   swaggerUrl: `${hostDomain}/api/v1/docs/swagger.json`,
+  //   explorer: true,
+  //   swaggerOptions: {
+  //     docExpansion: 'list',
+  //     filter: true,
+  //     showRequestDuration: true,
+  //   },
+  // });
+
+    const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .setSchemes(AppModule.isDev ? 'http' : 'https')
+    .setBasePath('/api/v1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api/v1/docs', app, document);
 
 
   if (module.hot) {
